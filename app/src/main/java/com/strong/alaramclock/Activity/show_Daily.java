57b549da214348b6720a99108ed423cb74a1ce33
SAYEED AJMAL <sayeedajmala06@gmail.com>
2023-02-14 16:45:04 +0530

    added Daily Alarm Showing

---
+++ app/src/main/java/com/strong/alaramclock/Activity/show_Daily.java
@@ -0,0 +1,67 @@
+package com.strong.alaramclock.Activity;
+
+import static com.strong.alaramclock.Activity.set_Bed.alarmManager;
+import static com.strong.alaramclock.services.play;
+
+import android.app.AlarmManager;
+import android.app.KeyguardManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.strong.alaramclock.databinding.ActivityDailyAlarmShowBinding;
+import com.strong.alaramclock.services;
+
+public class show_Daily extends AppCompatActivity {
+    ActivityDailyAlarmShowBinding Bind;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bind = ActivityDailyAlarmShowBinding.inflate(getLayoutInflater());
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+            KeyguardManager manager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+            manager.requestDismissKeyguard(this, null);
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        }
+        setContentView(Bind.getRoot());
+
+        Bind.CancelAlarm.setOnClickListener(view -> {
+            Intent intent = new Intent(this, services.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(show_Daily.this, 123, intent, PendingIntent.FLAG_IMMUTABLE);
+            if (play != null) {
+                if (alarmManager == null) {
+                    alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                }
+                play.stop();
+                alarmManager.cancel(pendingIntent);
+                Toast.makeText(this, "Alarm Stopped", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
